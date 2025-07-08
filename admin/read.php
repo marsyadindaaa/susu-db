26cb364b1be4902060698b7ab809e8f3a0ee33c8
Marsya adinda isyara br. saragih <adindamarsya33@gmail.com>
2025-07-08 11:03:43 +0700

    coba

--- admin/read.php
+++ admin/read.php
@@ -6,8 +6,7 @@ if (!isset($_SESSION['login'])) {
   header("Location: login.php");
   exit();
 }
-?>
-<?php
+
 include 'config.php';
 
 $result = $conn->query("SELECT * FROM produk");
@@ -27,8 +26,8 @@ while ($row = $result->fetch_assoc()) {
     <td>" . $row['nama_produk'] . "</td>
     <td>" . $row['harga'] . "</td>
     <td>
-      <a href='edit.php?id=" . $row['id'] . "'>Edit</a> |
-      <a href='delete.php?id=" . $row['id'] . "' onclick=\"return confirm('Hapus produk ini?')\">Hapus</a>
+      <a href='admin/edit.php?id=" . $row['id'] . "'>Edit</a> |
+      <a href='admin/delete.php?id=" . $row['id'] . "' onclick=\"return confirm('Hapus produk ini?')\">Hapus</a>
     </td>
   </tr>";
 }
